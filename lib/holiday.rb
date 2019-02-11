6db1c094c7ab0dbec5ea84669d6bb14ca9a8d7dc
Juan Alorro <jralorro93@gmail.com>
2019-02-11 20:29:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.select do |item|
       if item == "BBQ"
-        return season
+        return holiday 
+        binding.pry 
       end
     end 
   end 
