905c1ad437d60956ad2c7a357b2488618518ab8a
Juan Alorro <jralorro93@gmail.com>
2019-02-11 20:30:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,8 +69,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, holiday|
-    holiday.select do |item|
+  holiday_hash.collect do |season, holiday|
+    holiday.collect do |item|
       if item == "BBQ"
         return holiday 
       end
